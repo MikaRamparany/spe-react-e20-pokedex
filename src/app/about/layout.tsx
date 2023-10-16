28833e0eb85dc1be639ecaaa90e07747d2062638
Mika <110556435+MikaRamparany@users.noreply.github.com>
2023-10-16 14:05:34 +0200

    .

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -4,23 +4,21 @@
   → layouts imbriqués
 */
 
-import React from 'react';
-import type { Metadata } from 'next';
+import React from "react";
+import type { Metadata } from "next";
 
 /*
   Je peux ré-écrire les metadata
 */
 export const metadata: Metadata = {
-  title: 'About us | Pokédex',
-  description: 'Nous c\'est la promo Arancini !',
+  title: "About us | Pokédex",
+  description: "Nous c'est la promo Arancini !",
 };
 
 export default function AboutLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return (
-    <div className="about-layout">{children}</div>
-  );
+  return <div className="about-layout">{children}</div>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,12 @@
   de la page.
 */
 
-import './globals.css';
-import React from 'react';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import "./globals.css";
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 /*
   Par défaut, les composants Next sont rendus côté serveur
@@ -16,18 +16,20 @@ const inter = Inter({ subsets: ['latin'] });
   on peut renseigner ici les meta (title, description…).
 */
 export const metadata: Metadata = {
-  title: 'Pokédex',
-  description: 'Mon super Pokédex fabriqué avec NextJS',
+  title: "Pokédex",
+  description: "Mon super Pokédex fabriqué avec NextJS",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="fr">
-      <body className={inter.className}>
+      <body
+        className={`bg-cyan-950 flex flex-col min-h-screen text-white ${inter.className}`}
+      >
         {/*
           Le contenu de la page sera automatiquement donné via `children`
           au layout en fonction de l’arborescence de l'application.
@@ -35,7 +37,9 @@ export default function RootLayout({
           (équivalent de `<Outlet />` de React Router)
         */}
         {children}
-        <footer>© 2023 – O&apos;clock Arancini </footer>
+        <footer className="text-center text-cyan-50/50 py-2">
+          © 2023 – O&apos;clock Arancini
+        </footer>
       </body>
     </html>
   );
